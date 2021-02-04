90881eed2f28f97d239539888ea0c2125a2646ee
ridhambhat <ridham.bhat@gmail.com>
2021-02-05 02:32:10 +0530

    debug code - slate
    - mostly taken from last publish of teoh/slateVim fork
    - debug code with trials to correct insert_node.
    - not running merge_node causes a loop

--- src/App.js
+++ src/App.js
@@ -1,59 +1,33 @@
-// Import React dependencies.
-import React, { useMemo, useState, useCallback } from "react";
-// Import the Slate editor factory.
-import { createEditor, Transforms, Editor, Text, Path } from "slate";
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from "slate-react";
-import { withHistory } from "slate-history";
+import React, {Component} from "react";
 
 // Custom
 import Header from "./components/Header";
-import NormalEditor from "./components/NormalEditor";
-import InsertEditor from "./components/InsertEditor";
-import { btnStyle, paraStyle } from "./styles/tailwindStyles";
-import { INSERT_MODE, initialValue } from "./utils/variables";
+import SyncingEditor from "./components/SyncingEditor";
 
-const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState(initialValue);
-  const [mode, setMode] = useState(INSERT_MODE);
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <SyncingEditor />
+//       <div className="m-2"></div>
+//       <SyncingEditor />
+//     </div>
+//   );
+// };
 
-  const onModeButtonClick = (event) => {
-    setMode(event.target.value);
-  };
+// export default App;
 
-  return (
-    <div>
-      <Header />
-      <div className="container rounded bg-gray-200 p-2 w-1/6 mx-auto text-base text-center m-4">
-        <div className="flex space-x-4">
-          <button
-            className={`${btnStyle}`}
-            value="normal"
-            onClick={onModeButtonClick}
-          >
-            Normal
-          </button>
-          <button
-            className={`${btnStyle}`}
-            value="insert"
-            onClick={onModeButtonClick}
-          >
-            Insert
-          </button>
-        </div>
-      </div>
-      <p className={`${paraStyle}`}>
-        You are currently in{" "}
-        <span className="text-red-500">{mode.toUpperCase()}</span> mode.
-      </p>
-      {mode === INSERT_MODE ? (
-        <InsertEditor value={value} setValue={(value) => setValue(value)} />
-      ) : (
-        <NormalEditor value={value} setValue={(value) => setValue(value)} />
-      )}
-    </div>
-  );
+export default class App extends Component {
+  render(){
+    return(
+      <div>
+       <Header />
+       <SyncingEditor />
+       <div className="m-2"></div>
+       <SyncingEditor />
+     </div>
+    );
+  }
 };
 
-export default App;
+
---
+++ src/components/CombinedEditor.js
@@ -0,0 +1,75 @@
+import React, { Fragment, useState } from "react";
+import Mitt from "mitt";
+
+import NormalEditor from "./NormalEditor";
+import InsertEditor from "./InsertEditor";
+import { initialValue, INSERT_MODE, NORMAL_MODE } from "../utils/variables";
+import { btnStyle, paraStyle } from "../styles/tailwindStyles";
+
+const emitter = new Mitt();
+
+const CombinedEditor = ({ id, remote }) => {
+  const [value, setValue] = useState(initialValue);
+  const [mode, setMode] = useState(INSERT_MODE);
+
+  document.onkeydown = function(e){
+    if(e.key === "Escape" && mode === INSERT_MODE){
+      console.log("Escape");      
+      setMode(NORMAL_MODE);
+    }
+    else if(e.key === "i" && mode === NORMAL_MODE){
+      console.log("Insert");
+      setMode(INSERT_MODE);
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="container rounded bg-gray-200 p-2 w-1/6 mx-auto text-base text-center m-4">
+        <div className="flex space-x-4">
+          <button
+            className={`${btnStyle}`}
+            value="normal"
+            onClick={(e) => setMode(e.target.value)}
+          >
+            Normal
+          </button>
+          <button
+            className={`${btnStyle}`}
+            value="insert"
+            onClick={(e) => setMode(e.target.value)}
+          >
+            Insert
+          </button>
+        </div>
+      </div>
+      <p className={`${paraStyle}`}>
+        You are currently in {" "}  
+        <span className="text-red-500">{mode.toUpperCase()}</span> mode.
+      </p>
+      <p className={`${paraStyle}`}>
+        Box ID => {" "}  
+        <span className="text-red-500">{id.current.toString()}</span>.
+      </p>
+      {mode === INSERT_MODE ? (
+        <InsertEditor
+          id={id}
+          remote={remote}
+          emitter={emitter}
+          value={value}
+          setValue={(value) => setValue(value)}
+        />
+      ) : (
+        <NormalEditor
+          id={id}
+          remote={remote}
+          emitter={emitter}
+          value={value}
+          setValue={(value) => setValue(value)}
+        />
+      )}
+    </Fragment>
+  );
+};
+
+export default CombinedEditor;
