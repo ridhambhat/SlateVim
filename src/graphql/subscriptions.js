dd8a8f4714de3d9e1d104b0f702cdb9681c38b98
Zhixiang <zhixiangteoh@hotmail.com>
2021-02-06 17:00:19 -0500

    Initialize Amplify backend

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,66 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateDocument = /* GraphQL */ `
+  subscription OnCreateDocument {
+    onCreateDocument {
+      id
+      document
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateDocument = /* GraphQL */ `
+  subscription OnUpdateDocument {
+    onUpdateDocument {
+      id
+      document
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteDocument = /* GraphQL */ `
+  subscription OnDeleteDocument {
+    onDeleteDocument {
+      id
+      document
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateOperation = /* GraphQL */ `
+  subscription OnCreateOperation {
+    onCreateOperation {
+      id
+      editorId
+      op
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateOperation = /* GraphQL */ `
+  subscription OnUpdateOperation {
+    onUpdateOperation {
+      id
+      editorId
+      op
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteOperation = /* GraphQL */ `
+  subscription OnDeleteOperation {
+    onDeleteOperation {
+      id
+      editorId
+      op
+      createdAt
+      updatedAt
+    }
+  }
+`;
